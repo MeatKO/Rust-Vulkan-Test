d2c98074794bff121bcca356a64d86c9833a0171
meatko <bgrspot@gmail.com>
2022-12-04 23:25:10 +0200

    I tried so hard and got so far

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // #![feature(let_chains)]
 use cgmath::{Matrix3, Matrix4, Point3, Rad, Vector3};
 use ron::de;
-use std::{sync::Arc, time::Instant, arch::x86_64::_mm_movedup_pd};
+use std::{sync::Arc, time::Instant, arch::x86_64::_mm_movedup_pd, io::Cursor};
 use vulkano::{
     buffer::{
         allocator::{CpuBufferAllocator, CpuBufferAllocatorCreateInfo},
@@ -19,7 +19,7 @@ use vulkano::{
         QueueCreateInfo, QueueFlags,
     },
     format::Format,
-    image::{view::ImageView, AttachmentImage, ImageAccess, ImageUsage, SwapchainImage, swapchain},
+    image::{view::ImageView, AttachmentImage, ImageAccess, ImageUsage, SwapchainImage, swapchain, ImageDimensions, ImmutableImage, MipmapsCount},
     instance::{Instance, InstanceCreateInfo},
     memory::allocator::StandardMemoryAllocator,
     pipeline::{
@@ -28,6 +28,7 @@ use vulkano::{
             input_assembly::InputAssemblyState,
             vertex_input::BuffersDefinition,
             viewport::{Viewport, ViewportState}, render_pass,
+			color_blend::ColorBlendState
         },
         GraphicsPipeline, Pipeline, PipelineBindPoint,
     },
@@ -39,11 +40,12 @@ use vulkano::{
     },
     sync::{self, FlushError, GpuFuture},
     VulkanLibrary,
+	sampler::{Sampler, SamplerCreateInfo, SamplerAddressMode, Filter}
 };
 use vulkano::impl_vertex;
 use vulkano_win::VkSurfaceBuild;
 use winit::{
-    event::{Event, WindowEvent},
+    event::{Event, WindowEvent, MouseScrollDelta::LineDelta},
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
@@ -75,15 +77,16 @@ pub const VERTICES: [Vertex; 9] = [
 	Vertex { position: [-1.000000, -1.000000, 1.000000] }
 ];
 
-pub const NORMALS: [Normal; 8] = [
-	 Normal { normal: [-0.0000, 1.0000, -0.0000] },
-	 Normal { normal: [-0.0000, -0.0000, 1.0000] },
-	 Normal { normal: [-1.0000, -0.0000, -0.0000] },
-	 Normal { normal: [-0.0000, -1.0000, -0.0000] },
-	 Normal { normal: [1.0000, -0.0000, -0.0000] },
-	 Normal { normal: [-0.0000, -0.0000, -1.0000] },
-	 Normal { normal: [1.0000, -0.0000, -0.0000] },
-	 Normal { normal: [-0.0000, -0.0000, -1.0000] }
+pub const NORMALS: [Normal; 9] = [
+	Normal { normal: [1.000000, 1.000000, -1.000000] },
+	Normal { normal: [1.000000, 1.000000, -1.000000] },
+	Normal { normal: [1.000000, -1.000000, -1.000000] },
+	Normal { normal: [1.000000, 1.000000, 1.000000] },
+	Normal { normal: [1.000000, -1.000000, 1.000000] },
+	Normal { normal: [-1.000000, 1.000000, -1.000000] },
+	Normal { normal: [-1.000000, -1.000000, -1.000000] },
+	Normal { normal: [-1.000000, 1.000000, 1.000000] },
+	Normal { normal: [-1.000000, -1.000000, 1.000000] }
 ];
 
 pub const INDICES: [u16; 36] = [
@@ -231,7 +234,7 @@ fn main()
 	let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
 
 	// VERTEX BUFFER
-	let vertex_buffer = CpuAccessibleBuffer::from_iter(
+	let vertex_buffer = CpuAccessibleBuffer::<[Vertex]>::from_iter(
 		&memory_allocator, 
 		BufferUsage::VERTEX_BUFFER, 
 		false, 
@@ -292,29 +295,106 @@ fn main()
 	)
 	.unwrap();
 
-	let (mut pipeline, mut framebuffers) = window_size_dependent_setup(&memory_allocator, &vs, &fs, &images, render_pass.clone());
-	let mut recreate_swapchain = false;
-
 	let mut previous_frame_end = Some(sync::now(device.clone()).boxed());
 	let rotation_start = Instant::now();
 
-	let descritor_set_allocator = StandardDescriptorSetAllocator::new(device.clone());
+	let descriptor_set_allocator = StandardDescriptorSetAllocator::new(device.clone());
 	let command_buffer_allocator = StandardCommandBufferAllocator::new(device.clone(), Default::default());
 
-	event_loop.run(move |event, _, control_flow| 
+	let mut uploads = AutoCommandBufferBuilder::primary(
+		&command_buffer_allocator,
+		queue.queue_family_index(),
+		CommandBufferUsage::OneTimeSubmit
+	)
+	.unwrap();
+
+	// let png_bytes = include_bytes!("./../images/woag.png").to_vec();
+
+	
+    let texture = {
+		let png_bytes = include_bytes!("./../images/woag.png").to_vec();
+        let cursor = Cursor::new(png_bytes);
+        let decoder = png::Decoder::new(cursor);
+        let mut reader = decoder.read_info().unwrap();
+        let info = reader.info();
+        let dimensions = ImageDimensions::Dim2d {
+            width: info.width,
+            height: info.height,
+            array_layers: 1,
+        };
+        let mut image_data = Vec::new();
+        image_data.resize((info.width * info.height * 4) as usize, 0);
+        reader.next_frame(&mut image_data).unwrap();
+
+        let image = ImmutableImage::from_iter(
+            &memory_allocator,
+            image_data,
+            dimensions,
+            MipmapsCount::One,
+            Format::R8G8B8A8_SRGB,
+            &mut uploads,
+        )
+        .unwrap();
+		
+        ImageView::new_default(image).unwrap()
+    };
+
+	let sampler = Sampler::new(
+        device.clone(),
+        SamplerCreateInfo {
+            mag_filter: Filter::Linear,
+            min_filter: Filter::Linear,
+            address_mode: [SamplerAddressMode::Repeat; 3],
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
+	let (mut pipeline, mut framebuffers) = window_size_dependent_setup(&memory_allocator, &vs, &fs, &images, render_pass.clone());
+	let mut recreate_swapchain = false;
+
+	// let layout = pipeline.layout().set_layouts().get(0).unwrap();
+    // let texture_set = PersistentDescriptorSet::new(
+    //     &descriptor_set_allocator,
+    //     layout.clone(),
+    //     [WriteDescriptorSet::image_view_sampler(0, texture, sampler)],
+    // )
+    // .unwrap();
+
+	let mut scale_factor = 0.5f32;
+	let time_start = Instant::now();
+
+	event_loop.run(
+		move |event, _, control_flow| 
 		{
 			match event 
 			{
+				Event::WindowEvent { event: WindowEvent::MouseWheel { delta, .. }, .. } => 
+				{
+					match delta
+					{
+						LineDelta(_, delta) => 
+						{ 
+							scale_factor += delta * 0.05; 
+						}
+						_ => {}
+					}
+					println!("Delta : {:?}", delta);
+				}
+
 				Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => 
 				{
 					*control_flow = ControlFlow::Exit;
 				}
+
 				Event::WindowEvent { event: WindowEvent::Resized(_), .. } => 
 				{
 					recreate_swapchain = true;
 				}
+
 				Event::RedrawEventsCleared =>
 				{
+					scale_factor = (cgmath::Angle::sin(cgmath::Rad(Instant::now().duration_since(time_start).as_secs_f32() * 2.0f32)) * 0.4f32 + 1.0f32) / 2.0f32;
 					let window = surface.object().unwrap().downcast_ref::<Window>().unwrap();
 					let dimensions = window.inner_size();
 
@@ -327,6 +407,7 @@ fn main()
 
 					if recreate_swapchain
 					{
+						println!("swapchain recreation");
 						let (new_swapchain, new_images) =
 							match swapchain.recreate(
 								SwapchainCreateInfo {
@@ -372,7 +453,8 @@ fn main()
 							Point3::new(0.0, 0.0, 0.0),
 							Vector3::new(0.0, -1.0, 0.0),
 						);
-						let scale = Matrix4::from_scale(0.5);
+						let scale = Matrix4::from_scale(scale_factor);
+						// let scale = Matrix4::from_scale(0.5);
 
 						let uniform_data = vs::ty::Data {
 							world: Matrix4::from(rotation).into(),
@@ -384,12 +466,25 @@ fn main()
 					};
 
 					let layout = pipeline.layout().set_layouts().get(0).unwrap();
+
 					let set = PersistentDescriptorSet::new(
-						&descritor_set_allocator,
+						&descriptor_set_allocator,
 						layout.clone(),
-						[WriteDescriptorSet::buffer(0, uniform_buffer_subbuffer)]
+						[
+							WriteDescriptorSet::buffer(0, uniform_buffer_subbuffer)
+						]
 					)
 					.unwrap();
+
+					let texture_set = PersistentDescriptorSet::new(
+						&descriptor_set_allocator,
+						layout.clone(),
+						[
+							WriteDescriptorSet::image_view_sampler(0, texture.clone(), sampler.clone())
+						]
+					)
+					.unwrap();
+
 
 					let (image_index, suboptimal, acquire_future) =
 						match acquire_next_image(swapchain.clone(), None)
@@ -419,7 +514,7 @@ fn main()
 						.begin_render_pass(
 							RenderPassBeginInfo { 
 								clear_values: vec![
-									Some([0.0, 0.0, 1.0, 1.0].into()),
+									Some([0.0, 0.0, 0.0, 1.0].into()),
 									Some(1f32.into())
 								],
 								..RenderPassBeginInfo::framebuffer(
@@ -434,7 +529,13 @@ fn main()
 							PipelineBindPoint::Graphics, 
 							pipeline.layout().clone(), 
 							0, 
-							set
+							set.clone()
+						)
+						.bind_descriptor_sets(
+							PipelineBindPoint::Graphics, 
+							pipeline.layout().clone(), 
+							0, 
+							texture_set.clone()
 						)
 						.bind_vertex_buffers(0, (vertex_buffer.clone(), normals_buffer.clone()))
 						.bind_index_buffer(index_buffer.clone())
@@ -520,6 +621,7 @@ fn window_size_dependent_setup(
 		)
 		.collect::<Vec<_>>();
 
+	let subpass = Subpass::from(render_pass.clone(), 0).unwrap(); // h
 	let pipeline = GraphicsPipeline::start()
 		.vertex_input_state(
 			BuffersDefinition::new()
@@ -541,7 +643,9 @@ fn window_size_dependent_setup(
 		)
 		.fragment_shader(fs.entry_point("main").unwrap(), ())
 		.depth_stencil_state(DepthStencilState::simple_depth_test())
-		.render_pass(Subpass::from(render_pass, 0).unwrap())
+		.color_blend_state(ColorBlendState::new(subpass.num_color_attachments()).blend_alpha())
+        .render_pass(subpass)
+		// .render_pass(Subpass::from(render_pass, 0).unwrap())
 		.build(memory_allocator.device().clone())
 		.unwrap();
 
